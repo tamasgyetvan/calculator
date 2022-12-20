499cd3cfdb7e6c17d50a1d44170083bb79826c6f
tamasgyetvan <117265815+tamasgyetvan@users.noreply.github.com>
2022-12-20 12:31:10 +0100

    Add function to acBtn

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
+/**resultContainer in a variable */
+const resultContainer = document.querySelector(".resultContainer");
+
+/**Container for first number */
+let number1 = "";
+
+
 /**Event listener for calcBtn ID objects */
 
 let calcButtons = document.querySelectorAll(".calcBtn")
     .forEach(button => button.addEventListener("click", function(e) {
-        console.log(e.target.getAttribute("data-value"))
-
+        console.log(e.target.getAttribute("data-value"));
+        const dataValue = e.target.getAttribute("data-value");
+        appendValue(dataValue);
     }));
+
+/**Function to append button data-value to resultContainer */
+
+function appendValue(value) {
+    number1+=value;
+    resultContainer.innerHTML = number1;
+
+}
+
+/**Function to reset number1 and resultContainer's innerHTML, it clears everything from resultContainer */
+
+function clearResultContainer() {
+    number1 = "";
+    resultContainer.innerHTML = "0";
+
+}
+
+/**Clear button functioning */
+
+let cButton = document.getElementById("acBtn").addEventListener("click", function(e) {
+
+    clearResultContainer();
+})
